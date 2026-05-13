68bd16296be051ef76de9592228784707f931e44
KaashDev <seetohulkaash@gmail.com>
2026-05-13 16:39:09 -0400

    Randomizer

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+console.log("ROCK PAPER SCISSORS");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let rand = Math.random()
+
+    if(rand < (1/3)){
+        return("rock");
+    } else if(rand < (2/3)){
+        return("paper");
+    } else{
+        return("scissors");
+    }
+}
+
