d6ee4c3b2281c8a22612155ea66345c9592d4686
KaashDev <seetohulkaash@gmail.com>
2026-05-13 16:40:28 -0400

    Decide winner

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ function getHumanChoice(){
     return result;
 }
 
+function playRound(humanChoice, computerChoice){
+    
+    if(humanChoice === "rock"){
+        if(computerChoice === "rock"){
+            console.log("It's a Draw! You both picked Rock.");
+        } else if(computerChoice === "paper"){
+            console.log("You Lose! Paper beats Rock.");
+            computerScore++;
+        } else{
+            console.log("You Win! Rock beats Scissors.");
+            humanScore++;
+        }
+    }
+    if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            console.log("You Win! Paper beats Rock.");
+            humanScore++;
+        } else if(computerChoice === "paper"){
+            console.log("It's a Draw! You both picked Paper.");
+        } else{
+            console.log("You Lose! Scissors beats Paper");
+            computerScore++;
+        }
+    }
+
+    if(humanChoice === "scissors"){
+        if(computerChoice === "rock"){
+            console.log("You Lose! Rock beats Scissors.");
+            computerScore++;
+        } else if(computerChoice === "paper"){
+            console.log("You Win! Scissors beats Paper.");
+            humanScore++;
+        } else{
+            console.log("It's a Draw! You both picked Scissors.");
+        }
+    }
+}
+
